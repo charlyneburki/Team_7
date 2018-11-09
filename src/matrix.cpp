dbe18d8bab9600b1737d90ce9c1e2c835190958d
Elia Escoffier <eliaescoffier@tsf-460-wpa-2-206.epfl.ch>
2018-11-09 11:30:46 +0100

    Matrice qui compile

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,38 +1,42 @@
 #include "matrix.h"
 #include <cmath>
-
+using namespace std;
 
 Matrix::Matrix(){
 	for (size_t i(0); i < matrix.size() ; ++i){
-		for (size_t j(0); j < matrix[i].size() ; ++j){
+		for (size_t j(0); j < 4 ; ++j){
 			matrix[i][j] = 0;
 		}
 	}
 }
 
-Matrix::Matrix(vector <vector <double> > m){
-	matrix = m;
+Matrix::Matrix(vector < array <double, 4> > m){
+	for (size_t i(0); i < matrix.size() ; ++i){
+		for (size_t j(0); j < 4 ; ++j){
+			matrix[i][j] = m[i][j];
+		}
+	}
 }
 
 void Matrix::set_component(size_t x, size_t y, double poids){
 	matrix[x][y] = poids;
 }
 
-double score_calculation(string sequence){
+double Matrix::score_calculation(string sequence) const{
 	double score(0);
 	for (size_t i(0); i < sequence.length(); ++i){
 		switch (sequence[i]){
 			case 'A' : 
-			score = score + 2 + log2(matrix[0][i]);
+			score = score + 2 + log2(matrix[i][0]);
 			break;
 			case 'C' :
-			score = score + 2 + log2(matrix[1][i]);
+			score = score + 2 + log2(matrix[i][1]);
 			break;
 			case 'G' :
-			score = score + 2 + log2(matrix[2][i]);
+			score = score + 2 + log2(matrix[i][2]);
 			break;
 			case 'T' :
-			score = score + 2 + log2(matrix[3][i]);
+			score = score + 2 + log2(matrix[i][3]);
 			break;
 		}
 	}
--- src/matrix.h
+++ src/matrix.h
@@ -1,3 +1,8 @@
+#include <string>
+#include <vector>
+#include <array>
+using namespace std;
+
 /*!
  * This is a class modelising a matrix and the different calculations we can made with.
  */
@@ -7,7 +12,7 @@ class Matrix {
 	// C pour la deuxième colonne
 	// G pour la troisième colonne
 	// T pour la quatrième colonne
-	vector < vector <double> > matrix;
+	vector< array<double, 4> > matrix;
 	
 	public:
 	
@@ -19,7 +24,7 @@ class Matrix {
 	/*
 	 * @brief constructeur qui initialise la matrice avec des valeurs
 	 */
-	Matrix(vector <vector <double> > m);
+	Matrix(vector< array<double, 4> > m);
 	
 	/*
 	 * @brief Pour modifier une composante de la matrice
@@ -35,7 +40,7 @@ class Matrix {
 	 * 
 	 * @return le score en double de la séquence
 	 */
-	double score_calculation(string sequence);
+	double score_calculation(string sequence) const;
 	
 	/*
 	 * @brief accéder à une composante de la matrice
@@ -58,4 +63,4 @@ class Matrix {
 	bool accept(double threshold, string sequence) const;
 	
 	
-}
+};
